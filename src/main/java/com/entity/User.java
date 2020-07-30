e875957a1f51d9c959bc841feb322952a53e8fb6
codergo7 <codergo7@gmail.com>
2020-07-30 15:00:55 +0200

    added getter and setter methods and Constructor of USER class

--- src/main/java/com/entity/User.java
+++ src/main/java/com/entity/User.java
@@ -11,5 +11,33 @@ public class User {
 		this.password = password;
 		this.email = email;
 	}
-		
+	
+	public User(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
