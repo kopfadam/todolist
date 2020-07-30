d74897d04d3e2e8508571a0755651fe6b89a5bdc
codergo7 <codergo7@gmail.com>
2020-07-30 13:02:02 +0200

    added fields and constructor of User class

--- src/main/java/com/entity/User.java
+++ src/main/java/com/entity/User.java
@@ -1,5 +1,15 @@
 package com.entity;
 
 public class User {
-
+	
+	private String name;
+	private String password;
+	private String email;
+	
+	public User(String name, String password, String email) {
+		this.name = name;
+		this.password = password;
+		this.email = email;
+	}
+		
 }
