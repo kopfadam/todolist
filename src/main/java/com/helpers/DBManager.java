e12ec6cd58032ee5290df926a3ac2331dad1af63
Abdulvahap Karabekmez <abdulvahap.karabekmez@gmail.com>
2020-08-01 10:04:56 +0200

    updated DB manager

---
+++ src/main/java/com/helpers/DBManager.java
@@ -0,0 +1,29 @@
+package com.helpers;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBManager implements Serializable {
+	private static final long serialVersionUID = 1L;
+	Connection conn;
+
+
+	public DBManager(String url, String user, String pass) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(url, user, pass);
+		} catch (ClassNotFoundException | SQLException e) {
+		
+			e.printStackTrace();
+		}
+
+	}
+	
+	public Connection getConnection() {
+		
+		return conn;
+		
+	}
+}
