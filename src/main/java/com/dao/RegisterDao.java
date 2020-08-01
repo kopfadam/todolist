f6d08fc6701c94456d43169da22fd0ebed809864
Abdulvahap Karabekmez <abdulvahap.karabekmez@gmail.com>
2020-08-01 10:08:14 +0200

    Updated registerdao

--- src/main/java/com/dao/RegisterDao.java
+++ src/main/java/com/dao/RegisterDao.java
@@ -1,6 +1,7 @@
 package com.dao;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import com.helpers.DBManager;
 import com.entity.User;
@@ -30,6 +31,36 @@ public class RegisterDao {
 
 		return success;
 	}
+	
+	public Boolean userCheckDB(DBManager dbm, User user) {
+
+		boolean isRegistered = false;
+
+		
+		try {
+
+			String query = "SELECT Count(*) FROM user WHERE email=?";
+
+			PreparedStatement statement = dbm.getConnection().prepareStatement(query);
+			statement.setString(1, user.getEmail());
+
+			ResultSet rs = statement.executeQuery();
+
+			while (rs.next()) {
+				int numberOfRows = rs.getInt(1);
+				if (numberOfRows == 1) {
+					isRegistered = true;
+				} else if (numberOfRows == 0) {
+					isRegistered = false;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return isRegistered;
+	}
+	
 
 
 }
