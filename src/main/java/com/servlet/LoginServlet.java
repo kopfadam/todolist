a53c78ce666cd98571fe97a8782dc83d95a12e32
Adam Kopf <adamkopf0@gmail.com>
2020-08-02 16:55:09 +0300

    login operation logic implemented

--- src/main/java/com/servlet/LoginServlet.java
+++ src/main/java/com/servlet/LoginServlet.java
@@ -19,26 +19,54 @@ import com.helpers.Validator;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public LoginServlet() {
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doPost(request, response);
+	public LoginServlet() {
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		
-		
+		// get DBManager instance from servlet context
+		DBManager dbm = (DBManager) getServletContext().getAttribute("TodoDBManager");
+		User user = null;
+
+		// 1.if the user send his/her email and password as parameters via form, assign them to String variables
+		//   if a request comes without parameter send user to login page.
+		// 2.check if the email and password are both valid
+		// 3.call dao class' getUser() method that returns a user object after querying from the database.
+		// 4.if there is a registered user, set this object to session in order to make session operations easily.
+		// 5.redirect to tasks servlet.
+		if (!request.getParameterMap().isEmpty()) {
+			String email = request.getParameter("email");
+			String pass = request.getParameter("pass");
+			if (Validator.validateEmail(email) && Validator.validatePassword(pass)) {
+				user = LoginDao.getUser(dbm, email, pass);
+				if (user != null) {
+					request.getSession().setAttribute("user", user);
+					response.sendRedirect("user/tasks");
+				} else {
+					dispatcher(request, response, "Wrong email or password");
+				}
+			} else {
+				dispatcher(request, response, "Invalid email or password");
+			}
+		} else if (request.getSession().getAttribute("user") != null)
+			response.sendRedirect("index.jsp");
+		else
+			request.getRequestDispatcher("login.jsp").forward(request, response);
+
+	}
+
+	//a helper method to use for increasing readability
+	private void dispatcher(HttpServletRequest request, HttpServletResponse response, String message)
+			throws ServletException, IOException {
+		request.setAttribute("message", message);
+		request.getRequestDispatcher("login.jsp").forward(request, response);
 	}
 
 }
