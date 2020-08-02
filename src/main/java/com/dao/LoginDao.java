f7d6493b9baefead30dd2aebfb7af462d0b8bc93
Adam Kopf <adamkopf0@gmail.com>
2020-08-02 16:06:59 +0300

    created a dao class for login operations

--- src/main/java/com/dao/LoginDao.java
+++ src/main/java/com/dao/LoginDao.java
@@ -1,5 +1,38 @@
 package com.dao;
 
-public class LoginDao {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import org.mindrot.jbcrypt.BCrypt;
+
+import com.entity.User;
+import com.helpers.DBManager;
+
+public class LoginDao {
+	
+	//A method to check if a user is in the database. If finds a user, returns the user name and email. 
+	public static User getUser(DBManager dbm, String email, String pass) {
+		User user = null;
+		
+		String query = "select * from user where email=?";
+		
+		try {
+			PreparedStatement ps = dbm.getConnection().prepareStatement(query);
+			ps.setString(1, email);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				//use BCrypt checkpw() method to check if hash values are same
+				if(BCrypt.checkpw(pass, rs.getString("password"))) {
+					user = new User(rs.getNString("name"), rs.getString("email"));
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return user;
+	}
 }
--- src/main/java/com/servlet/LoginServlet.java
+++ src/main/java/com/servlet/LoginServlet.java
@@ -7,6 +7,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.dao.LoginDao;
+import com.entity.User;
+import com.helpers.DBManager;
+import com.helpers.Validator;
+
 /**
  * Servlet implementation class LoginServlet
  */
@@ -25,17 +30,15 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.getWriter().append("Served at goooo: ").append(request.getContextPath());
+		doPost(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		
 	}
 
 }
